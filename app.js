b4caae7683965a8a7e81f07b310aac24824f2b7d
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-03-15 01:15:43 +0600

    multer image uploaded

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
-
 const app = express();
+const ErrorHandler = require("./utils/ErrorHandler");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const fileUpload = require("express-fileupload");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use("/", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(fileUpload({ useTempFiles: true }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -8,4 +24,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     path: "/config/.env",
   });
 }
+
+// import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
+
+// error handling
+app.use(ErrorHandler);
+
 module.exports = app;
